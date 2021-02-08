61f2e791047076d3a96471dfa1640c96d3607f6e
Isabella Possaghi <isabella.possaghi@gmail.polimi.it>
2021-02-08 19:27:43 +0100

    yay

--- sketch.js
+++ sketch.js
@@ -1,13 +1,23 @@
-
+//move 01
 var config = {
-  position: 'start'
+  position: 'rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR'
 }
 var board = Chessboard('myBoard', config)
 
-var board2 = Chessboard('myBoard2', config)
-
-
 
 $('#m01').on('click', function () {
-  board.position('r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R')
+  board.position('rnbqkbnr/pp1ppppp/2p5/8/4P3/8/PPPP1PPP/RNBQKBNR')
+})
+
+
+
+//move 02
+var config2 = {
+  position: 'rnbqkbnr/pp1ppppp/2p5/8/3PP3/8/PPP2PPP/RNBQKBNR'
+}
+var board2 = Chessboard('myBoard2', config2)
+
+
+$('#m02').on('click', function () {
+  board2.position('rnbqkbnr/pp2pppp/2p5/3p4/3PP3/8/PPP2PPP/RNBQKBNR')
 })
